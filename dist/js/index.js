0712417cd9765884500c6bfb860717607cf120ce
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-07-15 12:19:33 +0200

    Assertions

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,26 @@
 "use strict";
-const favoriteFruits = ['apple', 'banana', 'strawberry'];
-const addFruit = (fruit) => {
-    favoriteFruits.push(fruit);
+// Type Assertions
+const mainHeading = document.getElementById('main-heading');
+// Convert to more or less speific
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more sppecific
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
 };
-addFruit('Cherry');
-console.log(favoriteFruits);
+const myVal = addOrConcat(2, 2, 'concat');
+// Be carefull! Typescript sees no problem - but a string is returned
+const nexVal = addOrConcat(2, 2, 'concat');
+10;
+// The DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('img');
+const nextImg = document.getElementById('img');
+img.src;
+myImg.src;
+nextImg.src;
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,36 @@
-const favoriteFruits: string[] = ['apple', 'banana', 'strawberry'];
+// Type Assertions
 
-const addFruit = (fruit: string) => {
-	favoriteFruits.push(fruit);
+const mainHeading = document.getElementById('main-heading') as HTMLElement;
+
+type One = string;
+type Two = string | number;
+type Three = 'hello';
+
+// Convert to more or less speific
+let a: One = 'hello';
+let b = a as Two; // less specific
+let c = a as Three; // more sppecific
+
+let d = <One>'world';
+let e = <string | number>'world';
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+	if (c === 'add') return a + b;
+	return '' + a + b;
 };
 
-addFruit('Cherry');
+const myVal: string = addOrConcat(2, 2, 'concat') as string;
 
-console.log(favoriteFruits);
+// Be carefull! Typescript sees no problem - but a string is returned
+const nexVal: number = addOrConcat(2, 2, 'concat') as number;
+
+10 as unknown as string;
+
+// The DOM
+const img = document.querySelector('img') as HTMLImageElement;
+const myImg = document.getElementById('img') as HTMLImageElement;
+const nextImg = <HTMLImageElement>document.getElementById('img');
+
+img.src;
+myImg.src;
+nextImg.src;
