9ff8e47d60df28aac901f0475b920f2d5e7a0d88
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-07-19 15:23:10 +0200

    19.07

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,20 @@
 "use strict";
+// Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+const prop = 'Pizza';
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
 //# sourceMappingURL=index.js.map
